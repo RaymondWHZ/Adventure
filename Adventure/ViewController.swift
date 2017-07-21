6f85b0038aca8c5fabe2fa720635b4aa0f29f319
iD Student <idstudent@stratton-two-thirty-two.mit.edu>
2017-07-21 13:24:13 -0400

    Finished end pictures and flood event.

--- Adventure/ViewController.swift
+++ Adventure/ViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+var haveRope=false
+var haveAxe=false
+var isMorning=false
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -19,9 +23,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var havePickaxe=false
-    var haveRope=false
-    var haveAxe=false
+    
+    @IBAction func gameStart(_ sender: Any) {
+        haveRope=false
+        haveAxe=false
+        isMorning=false
+    }
+    
+    @IBAction func getRope(_ sender: Any) {
+        haveRope=true
+    }
+    
+    @IBAction func getAxe(_ sender: Any) {
+        haveAxe=true
+    }
+    
+    @IBAction func overnight(_ sender: Any) {
+        isMorning=true
+    }
+    
+    @IBAction func headLeft(_ sender: Any) {
+        if haveAxe{
+            performSegue(withIdentifier: "Kill The Bear", sender: nil)
+        }
+        else{
+            performSegue(withIdentifier: "Killed By Bear", sender: nil)
+        }
+    }
+    
+    @IBAction func toThePassage(_ sender: Any) {
+        if isMorning{
+            performSegue(withIdentifier: "Flood", sender: nil)
+        }
+        else{
+            performSegue(withIdentifier: "Get Pickaxe", sender: nil)
+        }
+    }
+    
+    @IBAction func floodComes(_ sender: Any) {
+        if haveRope{
+            performSegue(withIdentifier: "Rope", sender: nil)
+        }
+        else if haveAxe{
+            performSegue(withIdentifier: "Axe", sender: nil)
+        }
+        else{
+            performSegue(withIdentifier: "Nothing", sender: nil)
+        }
+    }
 }
 
